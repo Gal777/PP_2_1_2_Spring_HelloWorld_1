9ba428bb5fbfe575c6d1c55f2f624488abdcc0ba
Gal777 <101939041+Gal777@users.noreply.github.com>
2022-06-05 21:49:10 +0300

    commit

--- src/main/java/AppConfig.java
+++ src/main/java/AppConfig.java
@@ -2,6 +2,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Scope;
 
+
 @Configuration
 public class AppConfig {
 
